594d6a0ab5d950d12b540e653f45e5a73a614a48
Vadan Khan <68273287+VadanKhan@users.noreply.github.com>
2023-03-23 11:06:00 +0000

    Improved Modelling for Lift and Drag

--- Halbach_Analysis.py
+++ Halbach_Analysis.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import scipy.constants as const
 
 # %% DECLARING CONSTANTS
-MU_0 = const.mu_0
+MU0 = const.mu_0
 
 # %% FUNCTION DEFINITIONS
 
@@ -165,9 +165,9 @@ def double_plot(graph_title, x_title, y_title, x_min, x_max, function_input1,
     return 0
 
 
-def Hu_liftforce(v, l, d, s, lam, M, y, h, mu, sig, Br):
+def Hu_maxforceplot(v, l, d, s, lam, M, y, h, mu, sig, Br):
     '''
-    lam = wavelength (length) of 1 periodic unit of halback array
+    lam = wavelength (length) of 1 periodic unit of halbach array
     k = 2pi / lam
     s = width of halbach
     l = length of halbach
@@ -175,11 +175,11 @@ def Hu_liftforce(v, l, d, s, lam, M, y, h, mu, sig, Br):
     y = distance from conductor plate to bottom of halbach
     M = number of magnets per wavelength
     h = height of conductor
-    mu = Relative permeability
+    mu = Permeability
     sig = Conductivity
     Br = Remnant Magnetisation
     '''
-    print(y)
+    print('seperation of halbach array (m):', y)
     # cond = 1-np.exp(-2*a*h)
     k = 2*np.pi / lam
     print("Halbach Wavenumber: ", k)
@@ -191,11 +191,85 @@ def Hu_liftforce(v, l, d, s, lam, M, y, h, mu, sig, Br):
     halheight = (1-np.exp(-k*d))**2
     print("Halbach Height Effect ", halheight)
     dist = np.exp(-2*k*y)
-    print("Distance Effect: ", dist)
+    print("Distance Effect: ", dist, "\n")
     velnum = v**2
     velden1 = (np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2))+1)**(3/2)
     velden2 = np.sqrt(np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2)) + 1)+np.sqrt(2)
-    return (consts * magres * halheight * dist * velnum)/(velden1 * velden2)
+
+    const_1 = s * l * Br / mu
+
+    return v/v * const_1 * magres * halheight * dist
+
+
+def Hu_liftforce(v, l, d, s, lam, M, y, h, mu, sig, Br):
+    '''
+    lam = wavelength (length) of 1 periodic unit of halbach array
+    k = 2pi / lam
+    s = width of halbach
+    l = length of halbach
+    d = height of halbach
+    y = distance from conductor plate to bottom of halbach
+    M = number of magnets per wavelength
+    h = height of conductor
+    mu = Permeability
+    sig = Conductivity
+    Br = Remnant Magnetisation
+    '''
+    print('seperation of halbach array (m):', y)
+    # cond = 1-np.exp(-2*a*h)
+    k = 2*np.pi / lam
+    print("Halbach Wavenumber: ", k)
+
+    consts = mu * sig**2 * s * l * Br**2 / k**2
+    print("Other Constants Effect: ", consts)
+    magres = (np.sinc(np.pi/M))**2
+    print("Magnetic Resolution Effect: ", magres)
+    halheight = (1-np.exp(-k*d))**2
+    print("Halbach Height Effect ", halheight)
+    dist = np.exp(-2*k*y)
+    print("Distance Effect: ", dist, "\n")
+    velnum = v**2
+    velden1 = (np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2))+1)**(3/2)
+    velden2 = np.sqrt(np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2)) + 1)+np.sqrt(2)
+
+    const_1 = s * l * Br / mu
+
+    return consts * magres * halheight * (velnum / (velden1*velden2)) * dist
+
+def Hu_dragforce(v, l, d, s, lam, M, y, h, mu, sig, Br):
+    '''
+    lam = wavelength (length) of 1 periodic unit of halbach array
+    k = 2pi / lam
+    s = width of halbach
+    l = length of halbach
+    d = height of halbach
+    y = distance from conductor plate to bottom of halbach
+    M = number of magnets per wavelength
+    h = height of conductor
+    mu = Permeability
+    sig = Conductivity
+    Br = Remnant Magnetisation
+    '''
+    print('seperation of halbach array (m):', y)
+    # cond = 1-np.exp(-2*a*h)
+    k = 2*np.pi / lam
+    print("Halbach Wavenumber: ", k)
+
+    consts = np.sqrt(2) * sig * s * l * Br**2 / k
+    print("Other Constants Effect: ", consts)
+    magres = (np.sinc(np.pi/M))**2
+    print("Magnetic Resolution Effect: ", magres)
+    halheight = (1-np.exp(-k*d))**2
+    print("Halbach Height Effect ", halheight)
+    dist = np.exp(-2*k*y)
+    print("Distance Effect: ", dist, "\n")
+    velnum = v
+    velden1 = (np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2))+1)
+    velden2 = np.sqrt(np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2)) + 1)+np.sqrt(2)
+
+    const_1 = s * l * Br / mu
+
+    return consts * magres * halheight * (velnum / (velden1*velden2)) * dist
 
 
 def create_plotm(graph_title, x_title, y_title, x_min, x_max, function_input,
@@ -205,8 +279,7 @@ def create_plotm(graph_title, x_title, y_title, x_min, x_max, function_input,
     Will plot a range of x values into a function. If the function requires
     more than just the x value input, it will try putting in extra_input1
     afterwards, and if not extra_input2 second, etc for further ei{} constants.
-    If neither works code will exit.
-
+    Otherwise code will exit.
     Returns
     -------
     0
@@ -232,6 +305,75 @@ def create_plotm(graph_title, x_title, y_title, x_min, x_max, function_input,
     plt.savefig(graph_title + '.png', dpi=777)
     plt.show()
     return 0
+
+
+def create_plotmm(graph_title, x_title, y_title, x_min, x_max, function_input,
+                  resolution, extra_input1, extra_input2, ei3, ei4, ei5, ei6,
+                  ei7, ei8, ei9, ei10, colour_input, function_input2):
+    """
+    Will plot a range of x values into a function. If the function requires
+    more than just the x value input, it will try putting in extra_input1
+    afterwards, and if not extra_input2 second, etc for further ei{} constants.
+    Otherwise code will exit.
+
+    Will then plot a second function.
+
+    Returns
+    -------
+    0
+
+    """
+
+    graph_title_consts = 'l={0:1.3g}, d={1:1.3g}, s={2:1.3g}, $\lambda$={3:1.3g},' \
+        ' M={4:1.3g}, y={5:1.3g}, $\mu_r$={7:1.3g}, $\sigma$={8:1.3g},' \
+        ' $B_r$={9:1.3g}'.format(
+            extra_input1, extra_input2, ei3, ei4, ei5, ei6, ei7, ei8/MU0, ei9, ei10)
+
+    fig, (axes, ax2) = plt.subplots(2)
+    fig.suptitle(graph_title_consts, fontsize=10)
+
+    axes.set_xlabel(x_title)
+    axes.set_ylabel(y_title)
+
+    x_values = np.linspace(x_min, x_max, resolution)
+
+    try:
+        axes.plot(x_values, function_input2(x_values, extra_input1,
+                  extra_input2, ei3, ei4, ei5, ei6,
+                  ei7, ei8, ei9, ei10), color='red', alpha=0.7,
+                  linestyle='--', label='Max Lift Force')
+        axes.legend(fontsize=8)
+    except Exception:
+        return 1
+
+    try:
+        axes.plot(x_values, function_input(x_values, extra_input1,
+                  extra_input2, ei3, ei4, ei5, ei6,
+                  ei7, ei8, ei9, ei10), color=colour_input)
+    except Exception:
+        return 1
+
+    ax2.set_xlabel(x_title)
+    ax2.set_ylabel(y_title)
+
+    x_values = np.linspace(x_min, x_max, resolution)
+
+    try:
+        ax2.plot(x_values, function_input(x_values, extra_input1,
+                                          extra_input2, ei3, ei4, ei5, ei6,
+                                          ei7, ei8, ei9, ei10),
+                 color=colour_input)
+    except Exception:
+        return 1
+    plt.tight_layout()
+    plt.savefig(graph_title + '.png', dpi=777)
+    plt.subplots_adjust(bottom=0.1,
+                        top=0.9,
+                        hspace=0.4)
+    plt.show()
+    return 0
+
+
 # %% MAIN
 
 
@@ -256,8 +398,8 @@ def main_code():
     """
     # %% plot all functions
     # double_plot('Lift to Drag against Velocity', 'Velocity (ms^-1)',
-    #             'Lift to Drag effect', 0, 20, lift_velocity_probe, 'Lift Effect',
-    #             drag_velocity_probe, 'Drag Effect', 250, 1,
+    #             'Lift to Drag effect', 0, 20, lift_velocity_probe,
+    #             'Lift Effect', drag_velocity_probe, 'Drag Effect', 250, 1,
     #             False, 'navy', 'red')
     # create_plot('Lift against Halbach array Height (array length = 50mm)',
     #             'Height (m)', 'Lift Effect', 0, 0.1, lift_against_height, 777,
@@ -275,24 +417,37 @@ def main_code():
     #             '(decay constant = 10)', 'Plate Thickness (m)',
     #             'Lift Effect', 0, 0.5, lift_against_height_conductor_thickness,
     #             777, 10, False, 'blue')
-    # create_plot('Lift against No. Magnets per Wavelength',
-    #             'No magnets', 'Lift Effect', 1, 20,
-    #             lift_against_magnet_resolution, 100, False, False, 'orange')
-    create_plotm('Lift Force against velocity',
-                 'velocity', 'Lift Force', 0, 20, Hu_liftforce, 777,
-                 1.6, 0.05, 0.3, 0.05, 8, 0.005, 1, 1.05 * MU_0, 10000000000, 1.3, 'navy')
-    # print(Hu_liftforce(10, 1.6, 1, 1, 0.05, 8, 0.1, 0.01, 1.05, 1/150E-6, 1.3))
-    # print(np.exp(-2*125*0.01))
-    # sig = 1/150E-6
-    # mu = 1.05
-    # k = 2 * np.pi / 0.05
-    # v = 1000000000000000000000000
-    # velnum = v**2
-    # velden1 = (np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2))+1)**(3/2)
-    # velden2 = np.sqrt(np.sqrt(1+(mu**2 * sig**2 * v**2)/(k**2)) + 1)+np.sqrt(2)
-    # ans = (velnum)/(velden1 * velden2)
-    # print(ans * mu * sig**2 / k**2)
-    # print(1/mu)
+    create_plot('Lift against No. Magnets per Wavelength',
+                'No magnets', 'Lift Effect', 1, 20,
+                lift_against_magnet_resolution, 100, False, False, 'orange')
+
+    '''
+    create_plotmm(graph_title, x_title, y_title, x_min, x_max, function_input,
+                     resolution, extra_input1, extra_input2, ei3, ei4, ei5, ei6,
+                     ei7, ei8, ei9, ei10, colour_input, function_input2):
+    '''
+    '''
+    Hu_liftforce or Humaxforceplot(v, l, d, s, lam, M, y, h, mu, sig, Br)
+    lam = wavelength (length) of 1 periodic unit of halbach array
+    k = 2pi / lam
+    s = width of halbach
+    l = length of halbach
+    d = height of halbach
+    y = distance from conductor plate to bottom of halbach
+    M = number of magnets per wavelength
+    h = height of conductor
+    mu = Permeability
+    sig = Conductivity
+    Br = Remnant Magnetisation
+    '''
+    create_plotmm('Lift Force against Velocity',
+                  'velocity', 'Lift Force', 0, 20, Hu_liftforce, 777, 1, 0.1,
+                  0.3, 0.4, 8, 0.01, 0.05, MU0*1.05, 2.2E6, 1.3, 'navy',
+                  Hu_maxforceplot)
+    create_plotmm('Drag Force against Velocity',
+                  'velocity', 'Drag Force', 0, 20, Hu_dragforce, 777, 1, 0.1,
+                  0.3, 0.4, 4, 0.01, 0.05, MU0*1.05, 2.2E6, 1.3, 'navy',
+                  Hu_maxforceplot)
     return 0
 
 
