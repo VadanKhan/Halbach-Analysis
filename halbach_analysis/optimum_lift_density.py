5efadca939019a84fb4c1f69bbd8c53a6d5366d2
Vadan Khan <vadankhan1@gmail.com>
2024-02-10 14:39:52 +0000

    reorganised plotting

--- halbach_analysis/optimum_lift_density.py
+++ halbach_analysis/optimum_lift_density.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from plotting_functions import create_plot
+from plotting_functions import array_plot
 # %% FUNCTION DEFINITIONS
 
 
@@ -15,83 +17,6 @@ def lift_against_halbach_thickness(halbach_thickness, halbach_wavenumber):
 def mass_against_halbach_thickness(halbach_thickness, mass_per_unit_length, other_mass):
     """mass of system against halbach thickness"""
     return halbach_thickness * mass_per_unit_length + other_mass
-
-
-def create_plot(function_input, x_min, x_max, resolution, *extra_inputs, y_min=None, y_max=None,
-                graph_title='title', x_title='x', y_title='y', colour_input='navy'):
-    """
-    Will plot a range of x values into a function. If the function requires
-    more than just the x value input, it will try putting in extra inputs one by one.
-    If none works, code will exit.
-
-    Returns
-    -------
-    0
-
-    """
-
-    fig = plt.figure()
-
-    axes = fig.add_subplot(111)
-
-    axes.set_title(graph_title)
-    axes.set_xlabel(x_title)
-    axes.set_ylabel(y_title)
-
-    if y_min is not None:
-        axes.set_ylim(bottom=y_min)
-    if y_max is not None:
-        axes.set_ylim(top=y_max)
-
-    x_values = np.linspace(x_min, x_max, resolution)
-
-    # Try to plot with each extra input until successful
-    for i in range(len(extra_inputs) + 1):
-        try:
-            axes.plot(x_values, function_input(
-                x_values, *extra_inputs[:i]), color=colour_input, label='Trend Prediction')
-            break
-        except Exception:
-            continue
-    else:
-        return 1
-
-    plt.savefig(graph_title + '.png', dpi=777)
-    return 0
-
-
-def array_plot(x_values, y_values, y_min=None, y_max=None, graph_title='title', x_title='x',
-               y_title='y', colour_input='navy'):
-    """
-    Will plot a range of x values and y values.
-
-    Returns
-    -------
-    0
-    """
-
-    fig = plt.figure()
-
-    axes = fig.add_subplot(111)
-
-    axes.set_title(graph_title)
-    axes.set_xlabel(x_title)
-    axes.set_ylabel(y_title)
-
-    if y_min is not None:
-        axes.set_ylim(bottom=y_min)
-    if y_max is not None:
-        axes.set_ylim(top=y_max)
-
-    try:
-        axes.plot(x_values, y_values, color=colour_input, label='Trend Prediction')
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return 1
-
-    plt.savefig(graph_title + '.png', dpi=777)
-    return 0
-
 
 # %% MAIN
 
